9ffe30d282d44027994a0b9a933299222bdd192d
MesumAli2 <smesumali20@gmail.com>
2023-05-23 02:05:59 +0400

    ui tests update

--- app/src/androidTest/java/com/example/tiptime/TipUiTests.kt
+++ app/src/androidTest/java/com/example/tiptime/TipUiTests.kt
@@ -1,5 +1,8 @@
 package com.example.tiptime
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTextInput
@@ -18,17 +21,21 @@ class TipUiTests {
     fun calculate_20_percent_tip() {
         composeTestRule.setContent {
             TipTimeTheme {
-                TipTimeLayout()
-                composeTestRule.onNodeWithText("Bill Amount")
-                    .performTextInput("10")
-                composeTestRule.onNodeWithText("Tip Percentage")
-                    .performTextInput("20")
-                val expectedTip = NumberFormat.getCurrencyInstance().format(2)
-                composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
-                    "No node with this text was found."
+                Surface (modifier = Modifier.fillMaxSize()) {
 
-                )
+                    TipTimeLayout()
+                }
             }
         }
+
+        composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Tip Percentage")
+            .performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+            "No node with this text was found."
+
+        )
     }
 }
